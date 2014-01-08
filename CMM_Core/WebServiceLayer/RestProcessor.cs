6998acc07fd32b87fa98555a3be16638aea2e83a
Jerrel Blankenship <jerrel@jerrelblankenship.com>
2014-01-08 15:24:55 -0500

    Working on rest api call now

---
+++ CMM_Core/WebServiceLayer/RestProcessor.cs
@@ -0,0 +1,20 @@
+﻿namespace CMM.WebServiceLayer
+{
+    using DomainLayer;
+    using RestSharp;
+
+    public class RestProcessor
+    {
+        public Country CountryInformation { get; set; }
+
+        public void GetCountry()
+        {
+            var client = new RestClient("http://restcountries.eu");
+
+            var request = new RestRequest("rest/capital/london", Method.GET);
+
+            IRestResponse response = client.Execute(request);
+            var content = response.Content;
+        }
+    }
+}
