4e263ce351ddae0ad441c3f55e536d555f6b3278
Jerrel Blankenship <jerrel@jerrelblankenship.com>
2014-01-08 14:57:45 -0500

    added basic repository

---
+++ CMM_Core/DataAccessLayer/DataRepository.cs
@@ -0,0 +1,43 @@
+﻿namespace CMM.DataAccessLayer
+{
+    using System;
+    using System.IO;
+    using DataLayer;
+    using DomainLayer;
+
+    public class DataRepository
+    {
+        internal string dbLocation = string.Empty;
+        internal CMMDatabase db = null;
+
+        public DataRepository()
+        {
+            dbLocation = GetFilePath();
+            db = new CMMDatabase(dbLocation);
+        }
+
+        public Country GetCountry(int id)
+        {
+            return db.GetItem<Country>(id);
+        }
+
+        public Country GetCountry(string capital)
+        {
+            return db.GetByCapital(capital);
+        }
+
+        private string GetFilePath()
+        {
+            const string dbName = "cmmDatabase.db3";
+
+#if __ANDROID__
+            string locationPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            var path = Path.Combine (locationPath, dbName);
+#else
+            var path = dbName;
+#endif
+            return path;
+
+        }
+    }
+}
