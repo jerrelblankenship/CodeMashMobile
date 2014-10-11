20c67e6974a73ee61fcad48f88375b0928fe9d1a
Jerrel Blankenship <jerrel@jerrelblankenship.com>
2014-10-11 07:12:58 -0400

    Some more changes

---
+++ Forms_App/Forms_App/Forms_App/Forms_App/App.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Forms_App
+{
+    public class App
+    {
+        public static Page GetMainPage()
+        {
+            var bControl = new Button
+            {
+                Text = "Click Me Please",
+            };
+
+            var lControl = new Label()
+            {
+                Text = "Hello, Forms !",
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+            };
+
+            return new ContentPage
+            {
+                Content = new StackLayout()
+                {
+                    Spacing = 10,
+                    VerticalOptions = LayoutOptions.End,
+                    Orientation = StackOrientation.Horizontal,
+                    HorizontalOptions = LayoutOptions.Start,
+                    Children = { lControl, bControl }
+                },
+            };
+        }
+    }
+}
